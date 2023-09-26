2e3964db2c9d6bb9729a1e6d79c62504073c0b71
Matheus Levi <matheuslevit@gmail.com>
2023-09-26 19:26:11 -0300

    Substituindo usando função

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/aula2.js
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/aula2.js
@@ -1,5 +1,11 @@
+// match - encontra ocorrências
+
 const { texto } = require('./base');
 
-const regExp1 = /João|Maria/gi;
+const regExp1 = /(João|Maria)/gi;
 
+console.log(texto);
 console.log(texto.match(regExp1));
+console.log(
+  texto.replace(regExp1, (input) => `### ${input.toUpperCase()} ###`),
+);
