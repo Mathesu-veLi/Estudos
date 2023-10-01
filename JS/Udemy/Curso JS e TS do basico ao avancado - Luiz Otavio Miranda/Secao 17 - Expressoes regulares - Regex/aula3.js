32a6848baa2d58766bc7126f4bf88b8e02b42b66
Matheus <matheuslevit@gmail.com>
2023-10-01 14:12:24 -0300

    Mostra palavras com minimo de letras 'e'

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/aula3.js
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/aula3.js
@@ -1,9 +1,13 @@
 /*
   Quantificadores:
-    * - 0 ou mais (opcional)
-    + - 1 ou mais (obrigatório)
-    ? - 0 ou 1 (opcional)
+    * - 0 ou mais {0, } (opcional)
+    + - 1 ou mais {1, } (obrigatório)
+    ? - 0 ou 1 {0, 1} (opcional)
     \ Caractere de escape
+    {mínimo, máximo} ( 
+      {10,} no mínimo 10
+      {,10} de 0 a 10
+    )
 */
 
 const { texto, arquivos } = require('./base');
@@ -13,7 +17,7 @@ const regExp1 = /Jo+ão+/gi;
 console.log(texto);
 console.log(texto.match(regExp1));
 
-const regExp2 = /\.(jpe?g)/gi;
+const regExp2 = /\.(jpe{0,}g)/gi;
 
 for (const arquivo of arquivos) {
   const válido = arquivo.match(regExp2);
