f71c989bb4aec33d170b8e5373dc4625a0f379a4
Matheus <matheuslevit@gmail.com>
2023-10-01 22:21:43 -0300

    Usa mínimo e máximo

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/aula3.js
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/aula3.js
@@ -1,9 +1,13 @@
 /*
   Quantificadores:
-    * (opcional)
-    + (obrigatório)
-    ? (opcional)
+    * - 0 ou mais {0, } (opcional)
+    + - 1 ou mais {1, } (obrigatório)
+    ? - 0 ou 1 {0, 1} (opcional)
     \ Caractere de escape
+    {mínimo, máximo} ( 
+      {10,} no mínimo 10
+      {,10} de 0 a 10
+    )
 */
 
 const { texto, arquivos } = require('./base');
@@ -13,9 +17,13 @@ const regExp1 = /Jo+ão+/gi;
 console.log(texto);
 console.log(texto.match(regExp1));
 
-const regExp2 = /\.(jpe?g)/gi;
+const regExp2 = /\.(jpe{0,}g)/gi;
 
 for (const arquivo of arquivos) {
+  const válido = arquivo.match(regExp2);
+
+  if(!válido) continue;
+
   console.log('');
   console.log(arquivo, arquivo.match(regExp2));
 }
