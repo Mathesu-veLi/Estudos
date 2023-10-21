d4d59087e77772b627a0b07adea0c238e1ed1133
Mathesu-veLi <matheuslevit@gmail.com>
2023-10-20 22:30:26 -0300

    Valida limite de ip

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/search_ip.js
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/search_ip.js
@@ -1,5 +1,5 @@
 const { ips } = require('./base')
 
-let regExp = /(\d+\.){3}\d+/g;
+let regExp = /((25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d)(\.)){3}(25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d)/g;
 
 console.log(ips.match(regExp))
