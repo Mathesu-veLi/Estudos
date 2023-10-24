3f67032bc8361946bb0370be38c3a3902d03466c
Mathesu-veLi <matheuslevit@gmail.com>
2023-10-23 21:06:48 -0300

    Cria regEx para usos futuros

---
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/Secao 17 - Expressoes regulares - Regex/aula10-receitas.js
@@ -0,0 +1,7 @@
+// Encontra todas as palavras (inclusive as acentuadas)
+const wordsRegEx = /[\wÀ-ú]+/gm;
+
+// Encontra números de telefone
+const phoneNumberRegEx = /^(?:\+\d{1,3})?\s?\(\d{2}\)\s?\d\s?\d{4}-\d{4}$/g;
+
+
