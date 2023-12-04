a83687a1c183d781f8a6f8845a3b56bc398cabc1
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-04 19:05:33 -0300

    Add values to variables after declaration

--- Go/Roadmap.sh/Learn the Basics/Variables in Go/variables.go
+++ Go/Roadmap.sh/Learn the Basics/Variables in Go/variables.go
@@ -5,19 +5,7 @@ import (
 )
 
 func main() {
-	var student1 string = "John" //type is string
-	var student2 = "Jane"        //type is inferred
-	x := 2                       //type is inferred
-
+	var student1 string
+	student1 = "John"
 	fmt.Println(student1)
-	fmt.Println(student2)
-	fmt.Println(x)
-
-	var a string
-	var b int
-	var c bool
-
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
 }
