38de39a78f4d06f92ef6483a388b0a6708f5c264
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-04 20:48:05 -0300

    Update main.go

--- Go/FullCycle/main.go
+++ Go/FullCycle/main.go
@@ -1,41 +1,13 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
-)
-
-type Course struct {
-	Name        string
-	Description string
-	Price       int
-}
-
-func (c Course) getFullInfo() string {
-	return fmt.Sprintf("Name: %s, Description: %s, Price: %d", c.Name, c.Description, c.Price)
-}
-
-func counter() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		time.Sleep(time.Second)
-	}
-}
+import "fmt"
 
 func main() {
-	go counter()
-	go counter()
-	counter()
-}
+	channel := make(chan string)
 
-func home(res http.ResponseWriter, req *http.Request) {
-	course := Course{
-		Name:        "Golang",
-		Description: "Golang Course",
-		Price:       100,
-	}
-	
-	json.NewEncoder(res).Encode(course)
+	go func () {
+		channel <- "Hello World!"
+	}()
+
+	fmt.Println(<-channel)
 }
