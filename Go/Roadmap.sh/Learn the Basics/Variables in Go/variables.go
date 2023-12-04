a18cb1084601e13aaca98823c8e9a73f83db7d42
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-04 19:12:31 -0300

    Declares multiple variables in a block

--- Go/Roadmap.sh/Learn the Basics/Variables in Go/variables.go
+++ Go/Roadmap.sh/Learn the Basics/Variables in Go/variables.go
@@ -5,10 +5,13 @@ import (
 )
 
 func main() {
-	var a, b, c, d int = 1, 3, 5, 7
+	var (
+		a int
+		b int = 1
+		c string = "hello"
+	  )
 
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
-	fmt.Println(d)
 }
