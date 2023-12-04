53a99ee505098876f77b8ec44743776458223d79
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-04 17:36:20 -0300

    Print a format string

--- Go/FullCycle/main.go
+++ Go/FullCycle/main.go
@@ -1,14 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
-	resultado, status := sum(10, 20)
-	println(resultado, status)
-}
-
-func sum(x int, y int) (int, bool) {
-	if x > 10 {
-		return x + y, true
-	}
-
-	return x + y, false
+	a := 10
+	fmt.Printf("Hello, %d world!", a)
 }
