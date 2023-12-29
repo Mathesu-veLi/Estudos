194cff85cdc8b1d7950714dc49b0cf38303eaef3
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-29 12:03:06 -0300

    Update A007-tipos-null-e-undefined.ts

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A007-tipos-null-e-undefined.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A007-tipos-null-e-undefined.ts
@@ -2,3 +2,15 @@ let x;
 if (typeof x === 'undefined') x = 20;
 console.log(x * 2);
 
+export function createPerson(
+    firstName: string,
+    lastName?: string
+): {
+    firstName: string;
+    lastName?: string;
+} {
+    return {
+        firstName,
+        lastName,
+    };
+}
