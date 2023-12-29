e8e3e534ac65b04d579de64ec8367d88f132b665
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-29 12:03:01 -0300

    Update A006-tuple.ts

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A006-tuple.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A006-tuple.ts
@@ -2,7 +2,8 @@ const dadosCliente1: readonly [number, string] = [1, 'Matheus'];
 const dadosCliente2: [number, string, string?] = [1, 'Matheus', 'Silva'];
 const dadosCliente3: [number, string, ...string[]] = [1, 'Matheus', 'Levi', 'Fernandes', 'da Silva'];
 
-dadosCliente1[0] = 100;
-dadosCliente1[1] = 'Carlos';
-
 console.log(dadosCliente1);
+console.log(dadosCliente2);
+console.log(dadosCliente3);
+
+const array: readonly string[] = ['Matheus', 'Otávio'];
