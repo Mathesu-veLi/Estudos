742d9a50771b5e3533a92e465357cadaf491385b
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-29 15:03:20 -0300

    Update A013-type-alias.ts

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A013-type-alias.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A013-type-alias.ts
@@ -9,3 +9,15 @@ type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
 type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
 type CorPreferida = CorRGB | CorCMYK;
 
+const pessoa: Pessoa = {
+    idade: 30,
+    nome: 'Matheus',
+    salario: 200_000,
+}
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+    return { ...pessoa, corPreferida: cor };
+}
+
+console.log(setCorPreferida(pessoa, 'Azul'));
+console.log(pessoa);
