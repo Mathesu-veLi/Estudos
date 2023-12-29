adda96997f105b9061b84440cb665f260d6a1d53
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-29 16:52:34 -0300

    Update A017-type-assertions.ts

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A017-type-assertions.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A017-type-assertions.ts
@@ -1,10 +1,8 @@
 // Conditional
+/* Recomendado */
 const body1 = document.querySelector('body');
 if (body1) body1.style.background = 'red';
 
-// Non-null assertion (!)
-const body2 = document.querySelector('body')!;
-body2.style.background = 'red';
 
 // Type assertion
 const body3 = document.querySelector('body') as HTMLBodyElement;
@@ -14,5 +12,11 @@ body3.style.background = 'red';
 const input = document.querySelector('.input') as HTMLInputElement;
 input.value = 'Qualquer coisa';
 input.focus();
+
+
+/* Não recomendado */
+// Non-null assertion (!)
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
 
 const body4 = (document.querySelector('body') as unknown) as number;
