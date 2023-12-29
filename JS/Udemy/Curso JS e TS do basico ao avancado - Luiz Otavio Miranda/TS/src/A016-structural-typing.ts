5eebeab22fe86b9bc550c5dc66abb926fe355805
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-29 16:33:06 -0300

    Create A016-structural-typing.ts

---
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A016-structural-typing.ts
@@ -0,0 +1,10 @@
+type VerifyUserFn = (user: User, receivedValue: User) => boolean;
+type User = { username: string; password: string };
+
+const verifyUser: VerifyUserFn = (user, receivedValue) => {
+    return user.username === receivedValue.username && user.password === receivedValue.password;
+};
+
+const dbUser = {username: 'joao', password: '123456'}
+const receivedUser = {username: 'joao', password: '123456'}
+const loggedIn = verifyUser(dbUser, receivedUser);
