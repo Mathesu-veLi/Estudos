4ddd00f5a5047784c0c1292a54d3a92889e7f389
Mathesu-veLi <matheuslevit@gmail.com>
2023-12-29 16:26:37 -0300

    Update A014-intersection-types.ts

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A014-intersection-types.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A014-intersection-types.ts
@@ -3,6 +3,11 @@ type TemSobrenome = { sobrenome: string };
 type TemIdade = { idade: number };
 type Pessoa = TemNome & TemSobrenome & TemIdade;
 
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type AD = 'A' | 'D';
+type Intersecao  = AB & AC & AD;
+
 export const pessoa: Pessoa = {
     idade: 30,
     nome: 'Matheus',
