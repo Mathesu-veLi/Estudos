35893f0e5bc0960d73f15e01df29ed4e87acd19b
Matheus <matheuslevit@gmail.com>
2024-01-10 20:41:47 -0300

    uses utility types

---
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0050-generics-8.ts
@@ -0,0 +1,27 @@
+const objeto1: Record<string, string | number> = {
+    nome: 'Matheus',
+    sobrenome: 'Levi',
+    idade: 13,
+};
+console.log(objeto1);
+
+export type PessoaProtocol = {
+    nome?: string;
+    sobrenome?: string;
+    idade?: number;
+};
+
+type PessoaRequired = Required<PessoaProtocol>;
+
+type PessoaPartial = Partial<PessoaRequired>;
+
+type PessoaReadonly = Readonly<PessoaRequired>;
+
+type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>;
+
+const objeto2: PessoaProtocol = {
+    nome: 'Matheus',
+    sobrenome: 'Levi',
+    idade: 13,
+};
+console.log(objeto2);
