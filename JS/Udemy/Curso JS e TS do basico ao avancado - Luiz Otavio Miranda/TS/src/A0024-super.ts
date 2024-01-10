b26638cde01d8ac4474ac1e442f969b14f5b288f
Matheus <matheuslevit@gmail.com>
2024-01-09 23:39:37 -0300

    copy files

---
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0024-super.ts
@@ -0,0 +1,39 @@
+export class Pessoa {
+    constructor(
+        public readonly nome: string,
+        public readonly sobrenome: string,
+        private idade: number,
+        protected readonly cpf: string,
+    ) {}
+
+    getIdade(): number {
+        return this.idade;
+    }
+
+    getCpf(): string {
+        return this.cpf;
+    }
+
+    getNomeCompleto(): string {
+        return `${this.nome} ${this.sobrenome}`;
+    }
+}
+
+export class Cliente extends Pessoa {
+    getNomeCompleto(): string {
+        return `Isso vem do cliente: ${this.nome} ${this.sobrenome}`;
+    }
+}
+export class Aluno extends Pessoa {
+    getNomeCompleto(): string {
+        return `Isso vem do aluno: ${this.nome} ${this.sobrenome}`;
+    }
+}
+
+const pessoa = new Pessoa('Matheus', 'Silva', 30, '000.000.000-00');
+const cliente = new Cliente('Maria', 'Veríssimo', 20, '111.111.111-11');
+const aluno = new Aluno('Luiz', 'Miranda', 40, '222.222.222-22');
+
+console.log(pessoa.getNomeCompleto());
+console.log(aluno.getNomeCompleto());
+console.log(cliente.getNomeCompleto());
