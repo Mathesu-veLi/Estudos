c5926c646335ee82808ac8aa4ee612e2ad12fe2b
Matheus <matheuslevit@gmail.com>
2024-01-10 19:48:49 -0300

    restrictions in generics

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0045-generics-3.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0045-generics-3.ts
@@ -16,12 +16,5 @@ const aluno2: PessoaProtocolo<string, number> = {
     idade: 13,
 };
 
-const aluno3: PessoaProtocolo<T = string, U = number> = {
-    nome: 'Matheus',
-    sobrenome: 'Levi',
-    idade: 13,
-};
-
 console.log(aluno1);
 console.log(aluno2);
-console.log(aluno3);
