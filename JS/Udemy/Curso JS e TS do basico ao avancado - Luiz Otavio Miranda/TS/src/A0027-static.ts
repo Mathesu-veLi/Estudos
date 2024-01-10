6ce0790d3e62193480bf5839762a7575f34d095f
Matheus <matheuslevit@gmail.com>
2024-01-10 00:09:18 -0300

    Creates a static function in a class

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0027-static.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0027-static.ts
@@ -5,8 +5,13 @@ export class Pessoa {
         public idade: number,
         public cpf: string,
     ) {}
+
+    static falaOi(): void {
+        console.log('Oi');
+    }
 }
 
 const pessoa = new Pessoa('Luiz', 'Miranda', 30, '000.000.0000-00');
 pessoa.cpf = '123.456.789.99';
 console.log(pessoa);
+Pessoa.falaOi();
