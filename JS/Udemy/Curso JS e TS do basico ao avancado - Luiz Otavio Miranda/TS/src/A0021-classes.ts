ea84e3c85cf397ba98a40458dd597f10d1865bbe
Matheus <matheuslevit@gmail.com>
2024-01-09 22:45:40 -0300

    creates array property and push items function

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0021-classes.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0021-classes.ts
@@ -1,14 +1,32 @@
 export class Empresa {
     public readonly nome: string;
+    private readonly colaboradores: Colaborador[] = [];
     protected readonly cnpj: string;
 
     constructor(nome: string, cnpj: string) {
         this.nome = nome;
         this.cnpj = cnpj;
     }
+
+    public adicionaColaborador(colaborador: Colaborador): void {
+        this.colaboradores.push(colaborador);
+    }
 }
 
-const empresa1 = new Empresa('Udemy');
+export class Colaborador {
+    constructor(
+        public readonly nome: string,
+        public readonly sobrenome: string,
+    ) {}
+}
 
 const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+
+const colaborador1 = new Colaborador('Matheus', 'Silva');
+const colaborador2 = new Colaborador('Luiz', 'Otávio');
+const colaborador3 = new Colaborador('João', 'Vieira');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+
 console.log(empresa1);
