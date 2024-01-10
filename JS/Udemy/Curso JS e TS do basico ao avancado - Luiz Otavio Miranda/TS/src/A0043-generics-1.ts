4f1e0dc2bd0bb5e506f7ac206b8e8f46061ebb06
Matheus <matheuslevit@gmail.com>
2024-01-10 15:30:35 -0300

    creates a filter with generics

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0043-generics-1.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0043-generics-1.ts
@@ -1,4 +1,26 @@
+type FilterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
+
+function meuFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
+    const novoArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (callbackfn(array[i], i, array)) {
+            novoArray.push(array[i]);
+        }
+    }
+
+    return novoArray;
+}
+
 export const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const arrayFiltradoOriginal = array.filter((value) => value < 5);
 console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = meuFilter(array, (value) => {
+    if (typeof value === 'number') {
+        return value < 5;
+    }
+    return false;
+});
+console.log(arrayFiltrado);
