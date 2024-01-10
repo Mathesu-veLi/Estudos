2e04b8eac133dfcbae9e5501e24217eb3fe05897
Matheus <matheuslevit@gmail.com>
2024-01-10 20:31:32 -0300

    uses type predicate

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0049-generics-7.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0049-generics-7.ts
@@ -1,6 +1,19 @@
-export function isNumber(value: unknown): boolean {
+export function isNumber(value: unknown): value is number {
     return typeof value === 'number';
 }
 
-console.log(isNumber('123'));
-console.log(isNumber(123));
+export function soma<T>(...args: T[]): number {
+    const retorno = args.reduce((sum, value) => {
+        if (isNumber(sum) && isNumber(value)) {
+            return sum + value;
+        }
+
+        return sum;
+    }, 0);
+
+    return retorno;
+}
+
+console.log(soma(1, 2, 3));
+console.log(soma(...[1, 2, 3, 'a', 'b', 'c', 1]));
+console.log(soma('a', 'b', 'c'));
