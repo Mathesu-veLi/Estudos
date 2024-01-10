d1e198c472dadb0d2be06536db110594fe9272ba
Matheus <matheuslevit@gmail.com>
2024-01-09 22:37:58 -0300

    creates a protected property

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0021-classes.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0021-classes.ts
@@ -1,10 +1,14 @@
 export class Empresa {
     public readonly nome: string;
+    protected readonly cnpj: string;
 
-    constructor(nome: string) {
+    constructor(nome: string, cnpj: string) {
         this.nome = nome;
+        this.cnpj = cnpj;
     }
 }
 
 const empresa1 = new Empresa('Udemy');
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
 console.log(empresa1);
