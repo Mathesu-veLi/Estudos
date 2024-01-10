bc3c61656615165443a8295ec894c63b4583f42e
Matheus <matheuslevit@gmail.com>
2024-01-10 20:14:45 -0300

    show the stack

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0047-generics-5.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0047-generics-5.ts
@@ -27,4 +27,22 @@ export class Pilha<T> {
     estaVazia(): boolean {
         return this.contador === 0;
     }
+
+    tamanho(): number {
+        return this.contador;
+    }
+
+    mostrarPilha(): void {
+        for (const chave in this.elementos) {
+            console.log(this.elementos[chave]);
+        }
+    }
 }
+
+const pilha = new Pilha<number>();
+
+pilha.push(1);
+pilha.push(2);
+pilha.push(3);
+pilha.push(4);
+pilha.mostrarPilha();
