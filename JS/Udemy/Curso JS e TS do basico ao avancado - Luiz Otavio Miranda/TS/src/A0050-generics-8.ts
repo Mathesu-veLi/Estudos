9ab2bc8e959f6b5b41bb65cdfe688616b44f31e9
Matheus <matheuslevit@gmail.com>
2024-01-10 20:54:19 -0300

    uses utility types pick

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0050-generics-8.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0050-generics-8.ts
@@ -30,3 +30,26 @@ type ABC = 'A' | 'B' | 'C';
 type CDE = 'C' | 'D' | 'E';
 type TipoExclude = Exclude<ABC, CDE>;
 type TipoExtract = Extract<ABC, CDE>;
+
+type AccountMongo = {
+    _id: string;
+    nome: string;
+    idade: number;
+};
+
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+    id: string;
+};
+
+const accountMongo: AccountMongo = {
+    _id: 'adw7a8dnwduawd8',
+    nome: 'Matheus',
+    idade: 18,
+};
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+    const { _id, ...accountData } = accountMongo;
+    return { ...accountData, id: _id };
+}
+
+const accountApi = mapAccount(accountMongo);
