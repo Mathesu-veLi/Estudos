10b0b22a95c2c79ea13dddb77ad59cc95ff36a97
Matheus <matheuslevit@gmail.com>
2024-01-10 13:58:49 -0300

    type guard verifing property of type

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0037-type-guards.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0037-type-guards.ts
@@ -5,17 +5,27 @@ export function add(a: unknown, b: unknown): number | string {
 console.log(add(1, 5));
 console.log(add('a', 'b'));
 
-type Pessoa = { nome: string };
-type Animal = { cor: string };
+type Pessoa = { tipo: 'pessoa'; nome: string };
+type Animal = { tipo: 'animal'; cor: string };
 type PessoaOuAnimal = Pessoa | Animal;
 
 export class Aluno implements Pessoa {
+    tipo = 'pessoa' as const;
     constructor(public nome: string) {}
 }
 
 function mostraNome(obj: PessoaOuAnimal): void {
     //if ('nome' in obj) console.log(obj.nome);
-    if (obj instanceof Aluno) console.log(obj.nome);
+    //if (obj instanceof Aluno) console.log(obj.nome);
+    switch (obj.tipo) {
+        case 'pessoa':
+            console.log(obj.nome);
+            break;
+        case 'animal':
+            console.log(obj.cor);
+            break;
+    }
 }
 
 mostraNome(new Aluno('João'));
+mostraNome({ tipo: 'animal', cor: 'rosa' });
