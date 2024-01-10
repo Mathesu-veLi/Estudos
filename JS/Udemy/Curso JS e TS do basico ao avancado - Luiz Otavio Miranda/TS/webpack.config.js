42246266ceb136208f79551afb8682a9d55e506e
Matheus <matheuslevit@gmail.com>
2024-01-09 22:03:59 -0300

    change webpack config

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/webpack.config.js
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/webpack.config.js
@@ -1,14 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/A0018-webpack/index.ts',
+    entry: './src/A0019-exercicio/index.ts',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                loader: 'ts-loader',
                 exclude: /node_modules/,
+                options: {
+                    configFile: 'tsconfig.frontend.json',
+                },
             },
         ],
     },
@@ -17,7 +21,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist', 'assets', 'js'),
+        path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
     },
     devtool: 'source-map',
 };
