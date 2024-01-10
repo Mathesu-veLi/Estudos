d6281b1786f2d98d6088600d405decb28175bfda
Matheus <matheuslevit@gmail.com>
2024-01-10 00:15:10 -0300

    creates static variables

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0027-static.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0027-static.ts
@@ -1,4 +1,7 @@
 export class Pessoa {
+    static idadePadrao = 0;
+    static cpfPadrao = '000.000.000-00';
+
     constructor(
         public nome: string,
         public sobrenome: string,
@@ -6,12 +9,17 @@ export class Pessoa {
         public cpf: string,
     ) {}
 
-    static falaOi(): void {
-        console.log('Oi');
+    metodoNormal(): void {
+        console.log(`${Pessoa.idadePadrao} ${Pessoa.cpfPadrao}`);
+    }
+
+    static criaPessoa(nome: string, sobrenome: string): Pessoa {
+        return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
     }
 }
 
-const pessoa = new Pessoa('Luiz', 'Miranda', 30, '000.000.0000-00');
-pessoa.cpf = '123.456.789.99';
-console.log(pessoa);
-Pessoa.falaOi();
+const pessoa1 = new Pessoa('Luiz', 'Miranda', 30, '000.000.0000-00');
+const pessoa2 = Pessoa.criaPessoa('Maria', 'Miranda');
+console.log(pessoa1);
+console.log(pessoa2);
+pessoa1.metodoNormal();
