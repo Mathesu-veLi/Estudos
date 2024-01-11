48ab8c39f33f48b3203d1dffeb0ce704025b33b6
Matheus <matheuslevit@gmail.com>
2024-01-10 23:07:29 -0300

    exports a class from a namespace

--- JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0060-namespaces/module.ts
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0060-namespaces/module.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 namespace MeuNamespace {
-    class PessoaDoNamespace {
+    export class PessoaDoNamespace {
         constructor(public nome: string) {}
     }
 
     const pessoa = new PessoaDoNamespace('Luiz');
     console.log(pessoa);
 }
+
+export const pessoa = new MeuNamespace.PessoaDoNamespace('Luiz');
+console.log(pessoa);
