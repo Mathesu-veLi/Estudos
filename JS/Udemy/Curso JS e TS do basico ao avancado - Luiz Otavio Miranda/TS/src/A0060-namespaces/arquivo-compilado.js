312b85d4b8c88a8a134b9516cc97e8eb593418a1
Matheus <matheuslevit@gmail.com>
2024-01-10 23:25:16 -0300

    import javascript file in a typescript code

---
+++ JS/Udemy/Curso JS e TS do basico ao avancado - Luiz Otavio Miranda/TS/src/A0060-namespaces/arquivo-compilado.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+var MeuNamespace;
+(function (MeuNamespace) {
+    MeuNamespace.nomeDoNamespace = 'Luiz';
+    var PessoaDoNamespace = /** @class */ (function () {
+        function PessoaDoNamespace(nome) {
+            this.nome = nome;
+        }
+        return PessoaDoNamespace;
+    }());
+    MeuNamespace.PessoaDoNamespace = PessoaDoNamespace;
+    MeuNamespace.pessoaDoNamespace = new PessoaDoNamespace('Luiz');
+    var OutroNamespace;
+    (function (OutroNamespace) {
+        OutroNamespace.nomeDoNamespace = 'Nome no outro namespace';
+    })(OutroNamespace = MeuNamespace.OutroNamespace || (MeuNamespace.OutroNamespace = {}));
+})(MeuNamespace || (MeuNamespace = {}));
+var constDoNamespace = 'Valor da const do namespace';
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="modulo/module.ts"/>
+console.log(MeuNamespace.pessoaDoNamespace);
