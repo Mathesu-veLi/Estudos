0cc1c89f51e5a71aef81567579e2554235e72ee6
Matheus Levi <matheuslevit@gmail.com>
2024-03-23 22:54:41 -0300

    read a number digited by user

--- Java/curso_nelio/src/curso_nelio/Main.java
+++ Java/curso_nelio/src/curso_nelio/Main.java
@@ -1,10 +1,19 @@
 package curso_nelio;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world!");
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		
+		x = sc.nextInt();
+		System.out.printf("Você digitou: %s", x);
+		
+		sc.close();
 	}
 
 }
