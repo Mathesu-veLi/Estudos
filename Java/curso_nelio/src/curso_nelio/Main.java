acbdfbac40cd4e684c4bc3a777ebafc1a4d8dfb4
Matheus Levi <matheuslevit@gmail.com>
2024-03-31 12:47:42 -0300

    edit conditional

--- Java/curso_nelio/src/curso_nelio/Main.java
+++ Java/curso_nelio/src/curso_nelio/Main.java
@@ -8,9 +8,9 @@ public class Main {
 		// TODO Auto-generated method stub
 		int x = 11;
 		
-		if (x <= 5) {
+		if (x < 12) {
 			System.out.println("Bom dia");
-		} else if (x <= 10) {
+		} else if (x < 18) {
 			System.out.println("Boa tarde");
 		} else {
 			System.out.println("Boa noite");
