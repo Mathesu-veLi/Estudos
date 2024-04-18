ed6f6fa67493ace9814979c5d335d35bd3e80bfe
Matheus Levi <matheuslevit@gmail.com>
2024-04-18 19:22:06 -0300

    creates a function to find the highest value

--- Java/curso_nelio/src/curso_nelio/Main.java
+++ Java/curso_nelio/src/curso_nelio/Main.java
@@ -5,16 +5,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x = 11;
+		Scanner sc = new Scanner(System.in);
 		
-		if (x < 12) {
-			System.out.println("Bom dia");
-		} else if (x < 18) {
-			System.out.println("Boa tarde");
-		} else {
-			System.out.println("Boa noite");
-		}
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int higher = max(a, b, c);
+		
+		showResult(higher);
+		
+		sc.close();
+	}
+	
+	public static int max(int number1, int number2, int number3) {
+		if (number1 > number2 && number1 > number3) return number1;
+		else if (number2 > number3) return number2;
+		else return number3;
+	}
+	
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
 	}
 
 }
