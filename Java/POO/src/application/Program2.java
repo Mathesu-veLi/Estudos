2303d99aa7ef841e0b6aacec6aea27966350844f
Matheus Levi <matheuslevit@gmail.com>
2024-04-18 20:30:45 -0300

    creates a product class, with add and remove methods

---
+++ Java/POO/src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program2 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product name: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.printf("name: %s\nprice: US$%.2f\nquantity: %d", product.name, product.price, product.quantity);
+		
+	}
+}
