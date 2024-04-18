4a5d0b57187d5477eba50cf3637688c48db2838d
Matheus Levi <matheuslevit@gmail.com>
2024-04-18 20:30:26 -0300

    rename program

--- Java/POO/src/application/Program1.java
+++ Java/POO/src/application/Program1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 import entities.Triangle;
 
-public class Program {
+public class Program1 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
