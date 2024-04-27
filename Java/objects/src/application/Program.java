6cca67a7387e75a4c6bfb5789854753d4db2b192
Matheus <matheuslevit@gmail.com>
2024-04-27 20:26:16 -0300

    create a vector of objects (products)

--- Java/objects/src/application/Program.java
+++ Java/objects/src/application/Program.java
@@ -3,21 +3,27 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        double[] vect = new double[n];
+        Product[] vect = new Product[n];
 
         for (int i = 0; i < n; i++) {
-            vect[i] = sc.nextDouble();
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+
+            vect[i] = new Product(name, price);
         }
 
         double sum = 0.0;
         for (int i = 0; i < n; i++) {
-            sum += vect[i];
+            sum += vect[i].getPrice();
         }
         double avg = sum / n;
 
