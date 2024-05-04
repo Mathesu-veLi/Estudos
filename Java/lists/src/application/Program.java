6327a57da5605be1b8fcd42ddd8ba41518bc6bd3
Matheus <matheuslevit@gmail.com>
2024-05-04 08:34:17 -0300

    use a filter to create a new list with names with 'a' in begin

--- Java/lists/src/application/Program.java
+++ Java/lists/src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
@@ -24,9 +26,13 @@ public class Program {
         for (String name : list) {
             System.out.println(name);
         }
-        System.out.println("-----------");
+        System.out.println("-------------");
         System.out.println("Index of Bob: " + list.indexOf("Bob"));
         System.out.println("Index of Marco: " + list.indexOf("Marco"));
-        
+        System.out.println("-------------");
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        for (String name : list) {
+            System.out.println(name);
+        }
     }
 }
