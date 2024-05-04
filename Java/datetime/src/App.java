e0cf89bbf7515bd4332c8c81fd07a5b6e7721051
Matheus <matheuslevit@gmail.com>
2024-05-04 19:14:20 -0300

    calculating days with the Instant class

--- Java/datetime/src/App.java
+++ Java/datetime/src/App.java
@@ -1,6 +1,8 @@
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,8 +10,8 @@ public class App {
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
         Instant d06 = Instant.parse("2002-07-20T01:30:26Z");
 
-        LocalDate pastWeekLocalDate = d04.minusDays(7);
-        LocalDate nextWeekLocalDate = d04.plusDays(7);
+        Instant pastWeekLocalDate = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekLocalDate = d06.plus(7, ChronoUnit.DAYS);
 
         System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
         System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
