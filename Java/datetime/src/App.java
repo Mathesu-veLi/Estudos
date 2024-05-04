e2e83e64d4a7c0e082912fd2897267331703762d
Matheus <matheuslevit@gmail.com>
2024-05-04 19:08:04 -0300

    calculating days

--- Java/datetime/src/App.java
+++ Java/datetime/src/App.java
@@ -1,8 +1,6 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,13 +8,10 @@ public class App {
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
         Instant d06 = Instant.parse("2002-07-20T01:30:26Z");
 
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        DateTimeFormatter fmt3 =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
 
-        System.out.println("d04 = " + d04.format(fmt1));
-        System.out.println("d05 = " + d05.format(fmt2));
-
-        System.out.println("d06 = " + fmt3.format(d06));
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
     }
 }
