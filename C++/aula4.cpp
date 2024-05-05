ef4bb11e2785aba84b336a0e0c649830b583c877
Matheus <matheuslevit@gmail.com>
2024-05-05 13:17:40 -0300

    declare variables and read a user response

---
+++ C++/aula4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int vidas = 0;
+    char letra = 'B';
+    double decimal = 5.2;
+    float decimal2 = 5.2;
+    bool vivo = true;
+    string nome = "Bruno";
+
+    cout << "Digite o número de vidas: ";
+    cin >> vidas;
+
+    cout << "Vidas: " << vidas << endl;
+
+    return 0;
+}
