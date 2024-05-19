b63068445bdb68f96262fab2d33045393e33a69b
Matheus <matheuslevit@gmail.com>
2024-05-18 23:31:04 -0300

    creates the on/off methods

--- C++/veli_led/veli_led.cpp
+++ C++/veli_led/veli_led.cpp
@@ -2,7 +2,16 @@
 #include <veli_led.h>
 
 veli_led::veli_led(int pin) {
-  this->pin = pin
+  this->pin = pin;
 
-  pinMode(this->pin, OUTPUT)
+  pinMode(this->pin, OUTPUT);
 }
+
+void veli_led::on() {
+  digitalWrite(this->pin, HIGH);
+}
+
+void veli_led::off() {
+  digitalWrite(this->pin, LOW);
+}
+
