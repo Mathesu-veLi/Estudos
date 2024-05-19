d95e4984ee69480ebc0a16040293d57898d16ffe
Matheus Levi <matheuslevit@gmail.com>
2024-05-18 22:33:46 -0300

    implements cm_distance function

--- C++/veli_ultrasonic/veli_ultrasonic.cpp
+++ C++/veli_ultrasonic/veli_ultrasonic.cpp
@@ -4,6 +4,23 @@
 veli_ultrasonic::veli_ultrasonic(int pinTrigger, int pinEcho) {
   this -> pinTrigger = pinTrigger;
   this -> pinEcho = pinEcho;
+
   pinMode(pinTrigger, OUTPUT);
-  pinMode(pinEcho)
+  pinMode(pinEcho, INPUT);
 }
+
+double veli_ultrasonic::cm_distance() {
+  digitalWrite(pinTrigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrigger, LOW);
+  unsigned long microseconds = pulseIn(pinEcho, HIGH);
+
+  double cm_distance = (microseconds * 0.0343) / 2.0;
+  if (cm_distance != 0 && cm_distance < 400) {
+    return cm_distance;
+  }
+  return -1.0;
+}
+
