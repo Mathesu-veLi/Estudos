e93807a333838780d594b100a75a4531a9a67882
Matheus <matheuslevit@gmail.com>
2024-06-07 20:50:33 -0300

    makes a downcasting with the classes

--- Java/inheritance/src/App.java
+++ Java/inheritance/src/App.java
@@ -1,5 +1,6 @@
 import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,8 +10,12 @@ public class App {
         // Upcasting
         Account acc1 = bacc;
         Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-        Account acc3 = new BusinessAccount(1004, "Anna", 0.0, 0.01);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
         
-        
+        // Downcasting
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(1000.00);
+        BusinessAccount acc5 = (BusinessAccount) acc3;
+        acc5.loan(1000.00);
     }
 }
