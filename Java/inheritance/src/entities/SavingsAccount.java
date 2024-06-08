7882624a1959285ce10ea2f5d6a88762d692f54c
Matheus <matheuslevit@gmail.com>
2024-06-07 21:17:43 -0300

    add final in a class and in a method

--- Java/inheritance/src/entities/SavingsAccount.java
+++ Java/inheritance/src/entities/SavingsAccount.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class SavingsAccount extends Account {
+public final class SavingsAccount extends Account {
   private Double interestRate;
 
   public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
@@ -21,7 +21,7 @@ public class SavingsAccount extends Account {
   }
 
   @Override
-  public void withdraw(double amount) {
+  public final void withdraw(double amount) {
     balance -= amount;
   }
 }
