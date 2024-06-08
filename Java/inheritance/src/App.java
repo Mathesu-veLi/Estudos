6fb31da2f5164d40f880424460f7f50b9606bcae
Matheus <matheuslevit@gmail.com>
2024-06-07 21:13:15 -0300

    overrides a method of the superclass in the subclass BusinessAccount

--- Java/inheritance/src/App.java
+++ Java/inheritance/src/App.java
@@ -1,4 +1,5 @@
 import entities.Account;
+import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class App {
@@ -10,5 +11,10 @@ public class App {
         Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
         acc2.withdraw(200.0);
         System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+
     }
 }
