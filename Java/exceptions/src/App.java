3d78e94d760f63bdee3ed1869097a9374583c712
Matheus <matheuslevit@gmail.com>
2024-06-09 18:23:05 -0300

    handle exception for array index

--- Java/exceptions/src/App.java
+++ Java/exceptions/src/App.java
@@ -4,9 +4,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        String[] vect = sc.nextLine().split(" ");
-        int position = sc.nextInt();
-        
-        System.out.println(vect[position]);
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+        }
+
+        System.out.println("End of program");
+
+        sc.close();
     }
 }
