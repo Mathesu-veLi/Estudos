2c2b29444bc008419c970150881d998b438916da
Matheus <matheuslevit@gmail.com>
2024-06-15 15:15:38 -0300

    creates the reservation class

---
+++ Java/exceptions/src/model/entities/Reservation.java
@@ -0,0 +1,52 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+  private Integer roomNumber;
+  private Date checkIn;
+  private Date checkOut;
+
+  private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+  
+  public Reservation() {
+  }
+
+  public Integer getRoomNumber() {
+    return roomNumber;
+  }
+
+  public void setRoomNumber(Integer roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  public Date getCheckIn() {
+    return checkIn;
+  }
+
+  public Date getCheckOut() {
+    return checkOut;
+  } 
+
+  public long duration() {
+    long diff = checkOut.getTime() - checkIn.getTime();
+    return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+  }
+
+  public void updateDates(Date checkIn, Date checkOut) {
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.format("Room: %d, ", roomNumber));
+    sb.append(String.format("check-in: %s, ", sdf.format(checkIn)));
+    sb.append(String.format("checkOut: %s, ", sdf.format(checkOut)));
+    sb.append(String.format("%d nights", sdf.format(duration())));
+    return sb.toString();
+  }
+}
