272992d7ea890cf7abba931e39c68ae91e5e4f3c
Matheus <matheuslevit@gmail.com>
2024-06-15 17:17:08 -0300

    remove error handlings of the main program

--- Java/exceptions/src/model/entities/Reservation.java
+++ Java/exceptions/src/model/entities/Reservation.java
@@ -61,7 +61,7 @@ public class Reservation {
     sb.append(String.format("Room: %d, ", roomNumber));
     sb.append(String.format("check-in: %s, ", sdf.format(checkIn)));
     sb.append(String.format("checkOut: %s, ", sdf.format(checkOut)));
-    sb.append(String.format("%o nights.", sdf.format(duration())));
+    sb.append(String.format("%s nights.", sdf.format(duration())));
     return sb.toString();
   }
 }
