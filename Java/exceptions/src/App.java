edfb2786e2eece3de626ee7c63f6a6265160befd
Matheus <matheuslevit@gmail.com>
2024-06-15 17:23:03 -0300

    uses try-catch block to handle error

--- Java/exceptions/src/App.java
+++ Java/exceptions/src/App.java
@@ -10,25 +10,31 @@ public class App {
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
 
-        System.out.print("Room number: ");
-        int number = sc.nextInt();
-        System.out.print("Check-in date(dd/MM/yyyy): ");
-        Date checkIn = sdf.parse(sc.next());
-        System.out.print("Check-out date (dd/MM/yyyy): ");
-        Date checkOut = sdf.parse(sc.next());
-
-        Reservation reservation = new Reservation(number, checkIn, checkOut);
-        System.out.println("Reservation: " + reservation);
-
-        System.out.println();
-        System.out.println("Enter data to update the reservation:");
-        System.out.print("Check-in date (dd/MM/yyyy): ");
-        checkIn = sdf.parse(sc.next());
-        System.out.print("Check-out date (dd/MM/yyyy): ");
-        checkOut = sdf.parse(sc.next());
-
-        reservation.updateDates(checkIn, checkOut);
-        System.out.println("Reservation: " + reservation);
+        try {
+            System.out.print("Room number: ");
+            int number = sc.nextInt();
+            System.out.print("Check-in date(dd/MM/yyyy): ");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(sc.next());
+    
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+    
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+    
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+        } catch (ParseException e) {
+            System.out.println("Invalid date format");
+        } catch(IllegalArgumentException e) {
+            System.out.println("Error in reservation: " + e.getMessage());
+        }
 
         sc.close();
     }
--- Java/exceptions/src/model/entities/Reservation.java
+++ Java/exceptions/src/model/entities/Reservation.java
@@ -44,10 +44,10 @@ public class Reservation {
   public String updateDates(Date checkIn, Date checkOut) {
     Date now = new Date();
     if (checkIn.before(now) || checkOut.before(now)) {
-      return "Reservation dates for updates must be future dates";
+      throw new IllegalArgumentException("Reservation dates for updates must be future dates");
     } 
     if (!checkOut.after(checkIn)) {
-      return "Check-out date must be after check-in date";
+      throw new IllegalArgumentException("Check-out date must be after check-in date");
     }
     
     this.checkIn = checkIn;
