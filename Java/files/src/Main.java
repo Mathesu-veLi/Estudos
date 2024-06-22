85389bb59a1f7d8f590b66e86ac239d65e49b07d
Matheus <matheuslevit@gmail.com>
2024-06-22 19:48:37 -0300

    list folders in a directory

--- Java/files/src/Main.java
+++ Java/files/src/Main.java
@@ -1,17 +1,20 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
   public static void main (String[] args) {
-    String path = "/home/veli/Documentos/out.txt";
-    String[] lines = new String[] {"oi", "aaa"};
+    Scanner sc = new Scanner(System.in);
 
-    try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-      for (String line : lines) {
-        bw.write(line);
-        bw.newLine();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    System.out.print("Enter a folder path: ");
+    String strPath = sc.nextLine();
+
+    File path = new File(strPath);
+    File[] folders = path.listFiles(File::isDirectory);
+    System.out.println("Folders: ");
+    for (File folder : folders) {
+      System.out.println(folder);
     }
+
+    sc.close();
   }
 }
