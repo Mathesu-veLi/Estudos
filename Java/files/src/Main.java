57c487c3c61decbd6e04e7c1a84f2b5a5bf59020
Matheus <matheuslevit@gmail.com>
2024-06-22 19:55:57 -0300

    get some informations about a file

--- Java/files/src/Main.java
+++ Java/files/src/Main.java
@@ -5,24 +5,15 @@ public class Main {
   public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("Enter a folder path: ");
+    System.out.print("Enter a file path: ");
     String strPath = sc.nextLine();
 
     File path = new File(strPath);
-    File[] folders = path.listFiles(File::isDirectory);
-    System.out.println("Folders: ");
-    for (File folder : folders) {
-      System.out.println(folder);
-    }
 
-    File[] files = path.listFiles(File::isFile);
-    System.out.println("Files: ");
-    for(File file : files) {
-      System.out.println(file);
-    }
+    System.out.println("getName: " + path.getName());
+    System.out.println("getParent: " + path.getParent());
+    System.out.println("getPath: " + path.getPath());
 
-    boolean success = new File(strPath + "\\subdir").mkdir();
-    System.out.println("Directory created successfully: " + success);
 
     sc.close();
   }
