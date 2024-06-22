419b1826fccee069181a68cad0050748d6db44dd
Matheus <matheuslevit@gmail.com>
2024-06-22 19:35:32 -0300

    read a file with FIleReader and BufferedReader

--- Java/files/src/Main.java
+++ Java/files/src/Main.java
@@ -1,22 +1,34 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
   public static void main (String[] args) {
-    File file = new File("/home/veli/Documentos/in.txt");
-    Scanner sc = null;
+    String path = "/home/veli/Documentos/in.txt";
+    FileReader fr = null;
+    BufferedReader br = null;
+
     try {
-      sc = new Scanner(file);
-      while (sc.hasNextLine()) {
-        System.out.println(sc.nextLine());
+      fr = new FileReader(path);
+      br = new BufferedReader(fr);
+
+      String line = br.readLine();
+
+      while(line != null) {
+        System.out.println(line);
+        line = br.readLine();
       }
-    } catch(FileNotFoundException e) {
+    } catch (Exception e) {
       System.out.println("Error: " + e.getMessage());
     } finally {
-      if (sc != null) {
-        sc.close();
+      try {
+        if (br != null) {
+          br.close();
+        }
+        if (fr != null) {
+          fr.close();
+        }
+      }
+      catch (IOException e) {
+        e.printStackTrace();
       }
     }
   }
