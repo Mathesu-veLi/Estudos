c4b80b74b2d59d0b2608064ddb09c3dcf1a9b112
Matheus <matheuslevit@gmail.com>
2024-06-22 19:42:59 -0300

    writes in a file

--- Java/files/src/Main.java
+++ Java/files/src/Main.java
@@ -2,17 +2,16 @@ import java.io.*;
 
 public class Main {
   public static void main (String[] args) {
-    String path = "/home/veli/Documentos/in.txt";
+    String path = "/home/veli/Documentos/out.txt";
+    String[] lines = new String[] {"oi", "aaa"};
 
-    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-      String line = br.readLine();
-
-      while(line != null) {
-        System.out.println(line);
-        line = br.readLine();
+    try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+      for (String line : lines) {
+        bw.write(line);
+        bw.newLine();
       }
-    } catch (Exception e) {
-      System.out.println("Error: " + e.getMessage());
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
