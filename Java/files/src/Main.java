2bc87630110b5c6add93bbf136139d5458afdc82
Matheus <matheuslevit@gmail.com>
2024-06-22 22:26:28 -0300

    read and copy file content to summary

--- Java/files/src/Main.java
+++ Java/files/src/Main.java
@@ -1,20 +1,44 @@
 import java.io.*;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
   public static void main (String[] args) {
+    Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("Enter a file path: ");
-    String strPath = sc.nextLine();
+    System.out.print("Path of file: ");
+    String stringPath = sc.nextLine();
+    File originalFile = new File(stringPath);
 
-    File path = new File(strPath);
+    try (BufferedReader originalFileReader = new BufferedReader(new FileReader(originalFile))) {
+      String line = originalFileReader.readLine();
+      String summaryPath = String.format("%s/out/summary.csv", originalFile.getParent());
 
-    System.out.println("getName: " + path.getName());
-    System.out.println("getParent: " + path.getParent());
-    System.out.println("getPath: " + path.getPath());
+      createFolderIfNoExist(originalFile.getParent());
 
+      try (BufferedWriter summary = new BufferedWriter(new FileWriter(summaryPath))) {
+        while (line != null) {
+          summary.write(line);
+          summary.newLine();
+          line = originalFileReader.readLine();
+        }
+      } catch (FileNotFoundException e) {
+        System.out.println("Error: " + e.getMessage());
+        System.out.println("2");
+      }
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
+      System.out.println("1");
+    }
 
     sc.close();
+  }
+
+  private static void createFolderIfNoExist (String folderParent) {
+    File folder = new File(folderParent + "/out");
+    if (!folder.exists()) {
+      folder.mkdir();
+    }
   }
 }
