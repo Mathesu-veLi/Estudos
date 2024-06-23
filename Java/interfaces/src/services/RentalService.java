085bbe08b49d54af19b9de39f75225c4f54c676e
Matheus <matheuslevit@gmail.com>
2024-06-23 19:20:14 -0300

    calculates basic payment

--- Java/interfaces/src/services/RentalService.java
+++ Java/interfaces/src/services/RentalService.java
@@ -3,6 +3,8 @@ package services;
 import entities.CarRental;
 import entities.Invoice;
 
+import java.time.Duration;
+
 public class RentalService {
   private Double pricePerHour;
   private Double pricePerDay;
@@ -16,6 +18,16 @@ public class RentalService {
   }
 
   public void processInvoice(CarRental carRental) {
+    double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+    double hours = minutes / 60.0;
+
+    double basicPayment;
+    if(hours <= 12.0) {
+      basicPayment = pricePerHour * Math.ceil(hours);
+    } else {
+      basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+    }
+
     carRental.setInvoice(new Invoice(50.0, 10.0));
   }
 }
