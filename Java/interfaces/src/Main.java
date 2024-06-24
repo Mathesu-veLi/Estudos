2d63feda1b421264c0a42fb2ebc081d360939c1c
Mathesu veLi <matheuslevit@gmail.com>
2024-06-24 14:11:51 -0300

    implements contract in the main program

--- Java/interfaces/src/Main.java
+++ Java/interfaces/src/Main.java
@@ -1,5 +1,28 @@
+import entities.Contract;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
   public static void main (String[] args) {
-    System.out.println("Hello world!");
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    System.out.println("Entre com os dados do contrato: ");
+    System.out.print("Número: ");
+    int number = sc.nextInt();
+    System.out.print("Data (dd/MM/yyyy): ");
+    LocalDate date = LocalDate.parse(sc.next(), fmt);
+    System.out.print("Valor do contrato: ");
+    double totalValue = sc.nextDouble();
+
+    Contract obj = new Contract(number, date, totalValue);
+
+    System.out.println("Entre com o número de parcelas: ");
+    int n = sc.nextInt();
   }
 }
