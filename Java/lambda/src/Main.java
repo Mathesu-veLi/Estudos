17e7ebd6c776d0aa17437f7ca5b2aa052bd97029
Mathesu veLi <matheuslevit@gmail.com>
2024-06-27 22:51:58 -0300

    transform the product name to uppercase with lambda in a method

--- Java/lambda/src/Main.java
+++ Java/lambda/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     list.add(new Product("Tablet", 350.00));
     list.add(new Product("HD Case", 80.90));
 
-    list.stream().map(Product::staticUpperCaseName).toList();
-    list.forEach(System.out::println);
+    List<String> names = list.stream().map(Product::nonStaticUpperCaseName).toList();
+    names.forEach(System.out::println);
   }
 }
