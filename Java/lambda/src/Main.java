5497e2094f08c37c72bc464feaa2d505aeae305e
Mathesu veLi <matheuslevit@gmail.com>
2024-06-27 21:47:57 -0300

    creates a list of products

--- Java/lambda/src/Main.java
+++ Java/lambda/src/Main.java
@@ -1,5 +1,19 @@
+import entites.Product;
+
+import java.util.*;
+
 public class Main {
   public static void main (String[] args) {
-    System.out.println("Hello world!");
+    Locale.setDefault(Locale.US);
+    List<Product> list = new ArrayList<>();
+
+    list.add(new Product("Tv", 900.00));
+    list.add(new Product("Mouse", 50.00));
+    list.add(new Product("Tablet", 350.00));
+    list.add(new Product("HD Case", 80.90));
+
+    for (Product p : list) {
+      System.out.println(p);
+    }
   }
 }
