4e42a35e03bd6315d8586c73043c222475c12157
Mathesu veLi <matheuslevit@gmail.com>
2024-06-27 22:12:11 -0300

    apply a price update in every list products with a declared lambda expression

--- Java/lambda/src/Main.java
+++ Java/lambda/src/Main.java
@@ -1,6 +1,7 @@
 import entites.Product;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class Main {
   public static void main (String[] args) {
@@ -12,7 +13,12 @@ public class Main {
     list.add(new Product("Tablet", 350.00));
     list.add(new Product("HD Case", 80.90));
 
-    list.forEach(Product::nonStaticPriceUpdate);
+    double factor = 1.1;
+
+    Consumer<Product> productConsumer = p -> p.setPrice(p.getPrice() * factor);
+
+
+    list.forEach(productConsumer);
     list.forEach(System.out::println);
   }
 }
