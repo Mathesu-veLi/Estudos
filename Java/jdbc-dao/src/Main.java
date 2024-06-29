20850544e16ceb60ac4cd08a5459fcd22b6ae2b3
Mathesu veLi <matheuslevit@gmail.com>
2024-06-28 21:55:41 -0300

    creates simple main program

--- Java/jdbc-dao/src/Main.java
+++ Java/jdbc-dao/src/Main.java
@@ -1,5 +1,12 @@
+import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
+
 public class Main {
   public static void main (String[] args) {
-    System.out.println("Hello world!");
+    Department obj = new Department(1, "Books");
+    Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+    System.out.println(seller);
   }
 }
