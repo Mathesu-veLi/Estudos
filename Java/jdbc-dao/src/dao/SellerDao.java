51603083a99cb5369f0fbd52220e5725e07f9ca4
Mathesu veLi <matheuslevit@gmail.com>
2024-06-29 01:00:51 -0300

    adds findBYDepartment in SellerDao

--- Java/jdbc-dao/src/dao/SellerDao.java
+++ Java/jdbc-dao/src/dao/SellerDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import entities.Department;
 import entities.Seller;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface SellerDao {
   Seller findById (Integer id);
 
   List<Seller> findAll ();
+
+  List<Seller> findByDepartment (Department department);
 }
