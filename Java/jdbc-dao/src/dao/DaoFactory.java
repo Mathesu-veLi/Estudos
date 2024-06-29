6e346327d94f6ec71f1cb3fbffeeaa0fb46cc92d
Mathesu veLi <matheuslevit@gmail.com>
2024-06-29 01:41:00 -0300

    creates a function to creates a DepartmentDaoImpl

--- Java/jdbc-dao/src/dao/DaoFactory.java
+++ Java/jdbc-dao/src/dao/DaoFactory.java
@@ -1,10 +1,15 @@
 package dao;
 
+import dao.impl.DepartmentDaoImpl;
 import dao.impl.SellerDaoImpl;
 import db.DB;
 
 public class DaoFactory {
   public static SellerDao createSellerDao () {
     return new SellerDaoImpl(DB.getConn());
+  }
+
+  public static DepartmentDao createDepartmentDao () {
+    return new DepartmentDaoImpl(DB.getConn());
   }
 }
