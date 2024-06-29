d4b7080910a3ed3e28d973ba0dcd0b649ee211db
Mathesu veLi <matheuslevit@gmail.com>
2024-06-29 01:45:47 -0300

    implements insert in DepartmentDaoImpl

--- Java/jdbc-dao/src/dao/impl/DepartmentDaoImpl.java
+++ Java/jdbc-dao/src/dao/impl/DepartmentDaoImpl.java
@@ -1,9 +1,11 @@
 package dao.impl;
 
 import dao.DepartmentDao;
+import db.DB;
+import db.DbException;
 import entities.Department;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
 public class DepartmentDaoImpl implements DepartmentDao {
@@ -15,7 +17,27 @@ public class DepartmentDaoImpl implements DepartmentDao {
 
   @Override
   public void insert (Department obj) {
+    PreparedStatement st = null;
+    try {
+      st = conn.prepareStatement("INSERT INTO department (Name) VALUES (?)");
+      st.setString(1, obj.getName());
 
+      int rowsAffected = st.executeUpdate();
+      if (rowsAffected > 0) {
+        ResultSet rs = st.getGeneratedKeys();
+        if (rs.next()) {
+          int id = rs.getInt(1);
+          obj.setId(id);
+        }
+        DB.closeResultSet(rs);
+      } else {
+        throw new DbException("Unexpected error! No rows affected!");
+      }
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeStatement(st);
+    }
   }
 
   @Override
