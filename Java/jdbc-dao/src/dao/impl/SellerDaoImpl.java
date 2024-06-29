ab8f34df94cfa46c2c15b5b632df64f04e179c81
Matheus <matheuslevit@gmail.com>
2024-06-29 00:39:13 -0300

    implements the findById function in Seller class

--- Java/jdbc-dao/src/dao/impl/SellerDaoImpl.java
+++ Java/jdbc-dao/src/dao/impl/SellerDaoImpl.java
@@ -33,7 +33,34 @@ public class SellerDaoImpl implements SellerDao {
 
   @Override
   public Seller findById (Integer id) {
-    return null;
+    PreparedStatement st = null;
+    ResultSet rs = null;
+
+    try {
+      st = conn.prepareStatement("SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE seller.Id = ?");
+      st.setInt(1, id);
+      rs = st.executeQuery();
+      if (rs.next()) {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+      }
+      return null;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeStatement(st);
+      DB.closeResultSet(rs);
+    }
   }
 
   @Override
