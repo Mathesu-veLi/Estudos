53f266de08eca3683dd29ed969423a029357074c
Matheus <matheuslevit@gmail.com>
2024-06-28 23:43:47 -0300

    creates and implements DaoFactory

--- Java/jdbc-dao/src/Main.java
+++ Java/jdbc-dao/src/Main.java
@@ -1,3 +1,5 @@
+import dao.DaoFactory;
+import dao.SellerDao;
 import entities.Department;
 import entities.Seller;
 
@@ -7,6 +9,7 @@ public class Main {
   public static void main (String[] args) {
     Department obj = new Department(1, "Books");
     Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+    SellerDao sellerDao = DaoFactory.createSellerDao();
     System.out.println(seller);
   }
 }
---
+++ Java/jdbc-dao/src/dao/DaoFactory.java
@@ -0,0 +1,9 @@
+package dao;
+
+import dao.impl.SellerDaoImpl;
+
+public class DaoFactory {
+  public static SellerDao createSellerDao () {
+    return new SellerDaoImpl();
+  }
+}
