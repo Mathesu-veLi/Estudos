690c5f608ffd30ff03605cd30cdf10c218792d22
Mathesu veLi <matheuslevit@gmail.com>
2024-06-30 20:12:04 -0300

    add the service annotation to the user service

--- Java/spring/src/main/java/com/veli/spring/services/UserService.java
+++ Java/spring/src/main/java/com/veli/spring/services/UserService.java
@@ -3,9 +3,11 @@ package com.veli.spring.services;
 import com.veli.spring.entities.User;
 import com.veli.spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
   @Autowired
   private UserRepository repository;
