2be6ecabd068ae36df3a161a4b77f249f1bba91f
Mathesu veLi <matheuslevit@gmail.com>
2024-06-30 20:10:35 -0300

    call user service on the user resource

--- Java/spring/src/main/java/com/veli/spring/resources/UserResource.java
+++ Java/spring/src/main/java/com/veli/spring/resources/UserResource.java
@@ -1,15 +1,22 @@
 package com.veli.spring.resources;
 
 import com.veli.spring.entities.User;
+import com.veli.spring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+  @Autowired
+  private UserService service;
+
   @GetMapping
-  public ResponseEntity<User> findAll () {
-    User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-    return ResponseEntity.ok().body(u);
+  public ResponseEntity<List<User>> findAll () {
+    List<User> list = service.findAll();
+    return ResponseEntity.ok().body(list);
   }
 }
