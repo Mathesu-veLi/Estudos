0a2dfcab95a131482915512791a9d8a64679bfc4
Mathesu veLi <matheuslevit@gmail.com>
2024-07-01 00:37:27 -0300

    creates a code to the order status

--- Java/spring/src/main/java/com/veli/spring/entities/enums/OrderStatus.java
+++ Java/spring/src/main/java/com/veli/spring/entities/enums/OrderStatus.java
@@ -1,5 +1,15 @@
 package com.veli.spring.entities.enums;
 
 public enum OrderStatus {
-  WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+  WAITING_PAYMENT(1), PAID(2), SHIPPED(3), DELIVERED(4), CANCELED(5);
+
+  private int code;
+
+  private OrderStatus (int code) {
+    this.code = code;
+  }
+
+  public int getCode () {
+    return code;
+  }
 }
