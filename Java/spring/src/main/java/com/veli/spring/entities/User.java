a71ce72046ec9074cca1556dc651168dd5bc0c81
Mathesu veLi <matheuslevit@gmail.com>
2024-07-01 00:03:02 -0300

    implements order class in the user class

--- Java/spring/src/main/java/com/veli/spring/entities/User.java
+++ Java/spring/src/main/java/com/veli/spring/entities/User.java
@@ -3,6 +3,8 @@ package com.veli.spring.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -14,6 +16,9 @@ public class User implements Serializable {
   private String email;
   private String phone;
   private String password;
+
+  @OneToMany(mappedBy = "client")
+  private List<Order> orders = new ArrayList<>();
 
   public User () {
   }
@@ -64,6 +69,10 @@ public class User implements Serializable {
 
   public void setPassword (String password) {
     this.password = password;
+  }
+
+  public List<Order> getOrders () {
+    return orders;
   }
 
   @Override
