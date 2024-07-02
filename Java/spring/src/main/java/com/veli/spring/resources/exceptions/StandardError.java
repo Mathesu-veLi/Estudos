be01def5b6b477e064425e4f70be982c83cff8c5
Matheus <matheuslevit@gmail.com>
2024-07-02 16:52:05 -0300

    format instant in StandardError class

--- Java/spring/src/main/java/com/veli/spring/resources/exceptions/StandardError.java
+++ Java/spring/src/main/java/com/veli/spring/resources/exceptions/StandardError.java
@@ -1,9 +1,12 @@
 package com.veli.spring.resources.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.time.Instant;
 
 public class StandardError implements Serializable {
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
   private Instant timeStamp;
   private Integer status;
   private String error;
