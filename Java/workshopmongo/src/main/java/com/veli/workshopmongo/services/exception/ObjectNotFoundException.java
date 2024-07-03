1f2d3ca48a1da8180feb8ef44c940734c6252233
Mathesu veLi <matheuslevit@gmail.com>
2024-07-02 21:47:54 -0300

    creates an object not found exception

---
+++ Java/workshopmongo/src/main/java/com/veli/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.veli.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+  public ObjectNotFoundException (String msg) {
+    super(msg);
+  }
+}
