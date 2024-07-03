c49c8434d77799ca12c4dbdf94ba36e179296e8b
Mathesu veLi <matheuslevit@gmail.com>
2024-07-02 21:22:21 -0300

    add the id annotation in the id property

--- Java/workshopmongo/src/main/java/com/veli/workshopmongo/domain/User.java
+++ Java/workshopmongo/src/main/java/com/veli/workshopmongo/domain/User.java
@@ -1,5 +1,6 @@
 package com.veli.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -7,6 +8,8 @@ import java.util.Objects;
 
 @Document
 public class User implements Serializable {
+
+  @Id
   private String id;
   private String name;
   private String email;
