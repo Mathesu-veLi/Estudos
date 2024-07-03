a47ba78da567c0a31b56adc3a9f59d87855b306f
Mathesu veLi <matheuslevit@gmail.com>
2024-07-03 00:31:03 -0300

    creates an endpoint to get the posts of a user

--- Java/workshopmongo/src/main/java/com/veli/workshopmongo/resources/UserResource.java
+++ Java/workshopmongo/src/main/java/com/veli/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.veli.workshopmongo.resources;
 
+import com.veli.workshopmongo.domain.Post;
 import com.veli.workshopmongo.domain.User;
 import com.veli.workshopmongo.dto.UserDTO;
 import com.veli.workshopmongo.services.UserService;
@@ -53,5 +54,12 @@ public class UserResource {
     obj.setId(id);
     obj = service.update(obj);
     return ResponseEntity.noContent().build();
+  }
+
+
+  @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+  public ResponseEntity<List<Post>> findPosts (@PathVariable String id) {
+    User obj = service.findById(id);
+    return ResponseEntity.ok().body(obj.getPosts());
   }
 }
