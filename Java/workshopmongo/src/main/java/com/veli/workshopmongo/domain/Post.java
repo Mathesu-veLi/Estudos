95d3357c45edd4430435eeacdb5a29259b101830
Mathesu veLi <matheuslevit@gmail.com>
2024-07-03 00:11:17 -0300

    add an author to post

--- Java/workshopmongo/src/main/java/com/veli/workshopmongo/domain/Post.java
+++ Java/workshopmongo/src/main/java/com/veli/workshopmongo/domain/Post.java
@@ -15,14 +15,17 @@ public class Post implements Serializable {
   private String title;
   private String body;
 
+  private User author;
+
   public Post () {
   }
 
-  public Post (String id, Date date, String title, String body) {
+  public Post (String id, Date date, String title, String body, User author) {
     this.id = id;
     this.date = date;
     this.title = title;
     this.body = body;
+    this.author = author;
   }
 
   public String getId () {
@@ -55,6 +58,14 @@ public class Post implements Serializable {
 
   public void setBody (String body) {
     this.body = body;
+  }
+
+  public User getAuthor () {
+    return author;
+  }
+
+  public void setAuthor (User author) {
+    this.author = author;
   }
 
   @Override
