bc76d53710ec1365af3596c3ba4c650dc893db84
Mathesu veLi <matheuslevit@gmail.com>
2024-07-02 23:02:59 -0300

    creates a function to find user by id

--- Java/workshopmongo/src/main/java/com/veli/workshopmongo/services/UserService.java
+++ Java/workshopmongo/src/main/java/com/veli/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.veli.workshopmongo.services;
 
 import com.veli.workshopmongo.domain.User;
 import com.veli.workshopmongo.repository.UserRepository;
+import com.veli.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,13 @@ public class UserService {
 
   public List<User> findAll () {
     return repo.findAll();
+  }
+
+  public User findById (String id) {
+    Optional<User> user = repo.findById(id);
+    if (user == null) {
+      throw new ObjectNotFoundException("Object not found");
+    }
+    return user.get();
   }
 }
