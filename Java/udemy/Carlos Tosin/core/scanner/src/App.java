b9bb1026a03c85a4e10094088483bedb56e33df9
Mathesu veLi <matheuslevit@gmail.com>
2025-01-25 23:43:17 -0300

    test scanner

---
+++ Java/udemy/Carlos Tosin/core/scanner/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+  public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String info1 = scanner.nextLine();
+    System.out.println(info1);
+  }
+}
