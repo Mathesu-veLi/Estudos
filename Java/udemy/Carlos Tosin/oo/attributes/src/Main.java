09d4055822936530c6d46c60b05793ec38978559
Matheus <matheuslevit@gmail.com>
2025-01-29 16:53:21 -0300

    creates a simple class to study attibutes

---
+++ Java/udemy/Carlos Tosin/oo/attributes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main (String[] args) {
+    Account a1 = new Account();
+    a1.accountNumber = "1234-5";
+
+    System.out.println(a1.accountNumber);
+  }
+}
