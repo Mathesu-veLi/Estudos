4d985f2cee3a8af37356dc187dbecce50d516542
Matheus <matheuslevit@gmail.com>
2025-02-05 22:13:32 -0300

    print a message if find errors

--- Java/udemy/Carlos Tosin/oo/oo-1/src/App2.java
+++ Java/udemy/Carlos Tosin/oo/oo-1/src/App2.java
@@ -2,11 +2,14 @@ public class App2 {
   public static void main (String[] args) {
     Account a = new Account();
 
-    a.deposit(100);
-    a.deposit(200);
 
-    a.withdraw(50);
+    if (!a.deposit(-100)) System.out.println("Ops, invalid deposit");
+    a.getBalance();
 
+    if (!a.deposit(200)) System.out.println("Ops, invalid deposit");
+    a.getBalance();
+
+    if (!a.withdraw(50)) System.out.println("Ops, invalid withdraw");
     a.getBalance();
   }
 }
