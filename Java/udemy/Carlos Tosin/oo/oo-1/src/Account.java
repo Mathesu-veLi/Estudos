cdf6d96de32f57422697cf01d5031991c397dc48
Matheus <matheuslevit@gmail.com>
2025-02-05 22:02:45 -0300

    creates methods in Account class to deposit and to get balance

--- Java/udemy/Carlos Tosin/oo/oo-1/src/Account.java
+++ Java/udemy/Carlos Tosin/oo/oo-1/src/Account.java
@@ -2,4 +2,12 @@ public class Account {
   String accountNumber;
   String accountOwner;
   double balance;
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+  void getBalance() {
+    System.out.println("Balance: " + balance);
+  }
 }
