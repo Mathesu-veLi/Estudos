bdd6e67e221236d380fbf3845db4dd5bf4dea001
Mathesu veLi <matheuslevit@gmail.com>
2025-02-21 11:44:37 -0300

    creates a overloading method to deposit method

--- Java/udemy/Carlos Tosin/oo/oo-1/src/Account.java
+++ Java/udemy/Carlos Tosin/oo/oo-1/src/Account.java
@@ -11,6 +11,10 @@ public class Account {
     return false;
   }
 
+  boolean deposit(String amount) {
+    return deposit(Double.parseDouble(amount));
+  }
+
   boolean withdraw(double amount) {
     if (balance >= amount && amount > 0) {
       balance -= amount;
--- Java/udemy/Carlos Tosin/oo/oo-1/src/App3.java
+++ Java/udemy/Carlos Tosin/oo/oo-1/src/App3.java
@@ -1,7 +1,7 @@
 public class App3 {
   public static void main (String[] args) {
     Account a = new Account();
-    a.deposit(1000);
+    a.deposit("1000");
 
     Account b = new Account();
     b.deposit(100);
