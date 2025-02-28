c17d5f7de356d139ec906f3a834b67b4fc3f1ac7
Mathesu veLi <matheuslevit@gmail.com>
2025-02-28 13:16:49 -0300

    creates a lamp class (exercise)

---
+++ Java/udemy/Carlos Tosin/oo/oo-1/src/Lamp.java
@@ -0,0 +1,19 @@
+public class Lamp {
+  private boolean on;
+
+  public Lamp (boolean on) {
+    this.on = on;
+  }
+
+  public void showState () {
+    System.out.println("Lâmpada " + (on ? "ligada" : "desligada"));
+  }
+
+  public void turnOn () {
+    this.on = true;
+  }
+
+  public void turnOff() {
+    this.on = false;
+  }
+}
