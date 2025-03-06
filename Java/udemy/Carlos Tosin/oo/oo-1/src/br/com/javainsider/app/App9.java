3c4456ae77c062ce53892e117ba2b7b1db087915
Mathesu veLi <matheuslevit@gmail.com>
2025-03-06 02:24:07 -0300

    import account classes

---
+++ Java/udemy/Carlos Tosin/oo/oo-1/src/br/com/javainsider/app/App9.java
@@ -0,0 +1,13 @@
+package br.com.javainsider.app;
+
+import br.com.javainsider.account.Account;
+import br.com.javainsider.account.EncapsulatedAccount;
+
+public class App9 {
+  public static void main (String[] args) {
+    Account a = new Account();
+    Account b = new Account();
+    Account c = new Account();
+    EncapsulatedAccount d = new EncapsulatedAccount("1234");
+  }
+}
