da7fc5312eb4356f8b2d1210862186ff7683bbdf
Mathesu veLi <matheuslevit@gmail.com>
2025-03-06 01:25:46 -0300

    creates a static id attribute in account

---
+++ Java/udemy/Carlos Tosin/oo/oo-1/src/IdGeneratedAccount.java
@@ -0,0 +1,58 @@
+public class IdGeneratedAccount {
+  private final String accountNumber;
+  private final String accountOwner;
+  private double balance;
+
+  private static int currentId = 1;
+
+  public IdGeneratedAccount (String accountOwner) {
+    this.accountNumber = "000" + currentId++;
+    this.accountOwner = accountOwner;
+    this.balance = 0;
+  }
+
+  boolean deposit (double amount) {
+    if (amount > 0) {
+      balance += amount;
+      return true;
+    }
+    return false;
+  }
+
+  boolean deposit(String amount) {
+    return deposit(Double.parseDouble(amount));
+  }
+
+  boolean withdraw(double amount) {
+    if (balance >= amount && amount > 0) {
+      balance -= amount;
+      return true;
+    }
+    return false;
+  }
+
+  void transfer (double amount, IdGeneratedAccount targetAccount) {
+    this.withdraw(amount);
+    targetAccount.deposit(amount);
+  }
+
+  void printBalance () {
+    System.out.println("Balance: " + balance);
+  }
+
+  public String getAccountNumber () {
+    return accountNumber;
+  }
+
+  public String getAccountOwner () {
+    return accountOwner;
+  }
+
+  public void setBalance (double balance) {
+    this.balance = balance;
+  }
+
+  public double getBalance () {
+    return balance;
+  }
+}
