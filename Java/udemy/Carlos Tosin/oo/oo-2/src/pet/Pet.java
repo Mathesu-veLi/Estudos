4ecf6f9020032e587e3e8fcf330ccbc53fe33439
Mathesu veLi <matheuslevit@gmail.com>
2025-03-09 17:47:28 -0300

    transform pet in an abstract class

--- Java/udemy/Carlos Tosin/oo/oo-2/src/pet/Pet.java
+++ Java/udemy/Carlos Tosin/oo/oo-2/src/pet/Pet.java
@@ -1,7 +1,8 @@
 package pet;
 
-public class Pet {
+public abstract class Pet {
   private String color;
+  private int amountEaten;
 
   public String getColor() {
     return color;
@@ -9,5 +10,13 @@ public class Pet {
 
   public void setColor(String color) {
     this.color = color;
+  }
+
+  public void feed(int amount) {
+    amountEaten += amount;
+  }
+
+  public int getAmountEaten() {
+    return amountEaten;
   }
 }
