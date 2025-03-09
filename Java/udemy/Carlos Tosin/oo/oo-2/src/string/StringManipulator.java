8f2a85b58afea2e3cf3d59d0a0372d2d100a5fa2
Mathesu veLi <matheuslevit@gmail.com>
2025-03-09 16:34:36 -0300

    default methods in interfaces

---
+++ Java/udemy/Carlos Tosin/oo/oo-2/src/string/StringManipulator.java
@@ -0,0 +1,10 @@
+package string;
+
+public interface StringManipulator {
+  String join(String s1, String s2);
+  String takeFirst(String s, int length);
+  default String upper(String s) {
+    return s.toUpperCase();
+  }
+  ;
+}
