3c6b5cb651ceff090ae2e4396a470ffa6625ced8
Mathesu veLi <matheuslevit@gmail.com>
2025-03-09 21:36:31 -0300

    catches error with if

---
+++ Java/udemy/Carlos Tosin/exceptions/src/account/Account1.java
@@ -0,0 +1,33 @@
+package account;
+
+public class Account1 {
+  private final String number;
+  private double balance;
+
+  public Account1(String number) {
+    this.number = number;
+  }
+
+  public void deposit(double amount) {
+    this.balance += amount;
+  }
+
+  public boolean withdraw(double amount) {
+    if(amount < 0) {
+      return false;
+    }
+
+    if(balance - amount >= 0) {
+      this.balance -= amount;
+      return true;
+    }
+
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "Account1{" + "number='" + number + '\'' + ", balance=" + balance +
+        '}';
+  }
+}
