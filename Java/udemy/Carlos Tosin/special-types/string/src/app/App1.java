8be5b649fd8de49c0538c336a48f8ae7d135a80c
Mathesu veLi <matheuslevit@gmail.com>
2025-03-11 23:02:24 -0300

    testing the string pool

---
+++ Java/udemy/Carlos Tosin/special-types/string/src/app/App1.java
@@ -0,0 +1,13 @@
+package app;
+
+public class App1 {
+  public static void main(String[] args) {
+    String s1 = "A";
+    String s2 = "B";
+    String s3 = "B";
+    String s4 = new String("B");
+
+    System.out.println(s2 == s3);
+    System.out.println(s4 == s3);
+  }
+}
