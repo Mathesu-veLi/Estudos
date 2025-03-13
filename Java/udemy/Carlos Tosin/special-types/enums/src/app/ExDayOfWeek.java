449d97049e2a4b72efda5f0692a2e7a3d470e41e
Mathesu veLi <matheuslevit@gmail.com>
2025-03-12 23:24:59 -0300

    realise exercise

---
+++ Java/udemy/Carlos Tosin/special-types/enums/src/app/ExDayOfWeek.java
@@ -0,0 +1,41 @@
+package app;
+
+public class ExDayOfWeek {
+  public enum DayOfWeek {
+    SUNDAY(null), SATURDAY(SUNDAY), FRIDAY(SATURDAY), THURSDAY(
+        FRIDAY), WEDNESDAY(THURSDAY), TUESDAY(WEDNESDAY), MONDAY(TUESDAY);
+
+    private DayOfWeek next;
+
+    DayOfWeek(DayOfWeek next) {
+      this.next = next;
+    }
+
+    public DayOfWeek next() {
+      return this.next == null ? MONDAY : this.next;
+    }
+  }
+
+  public static void main(String[] args) {
+    DayOfWeek dow = DayOfWeek.MONDAY;
+    System.out.println(dow);
+
+    dow = dow.next();
+    System.out.println(dow);
+
+    dow = dow.next();
+    System.out.println(dow);
+
+    dow = dow.next();
+    System.out.println(dow);
+
+    dow = dow.next();
+    System.out.println(dow);
+
+    dow = dow.next();
+    System.out.println(dow);
+
+    dow = dow.next();
+    System.out.println(dow);
+  }
+}
