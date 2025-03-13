5dcf9391f6b14da90237a63c35d45dd8cd71db4c
Matheus <matheuslevit@gmail.com>
2025-03-12 22:56:22 -0300

    uses attributes, methods and constructors in enums

---
+++ Java/udemy/Carlos Tosin/special-types/enums/src/app/App2.java
@@ -0,0 +1,11 @@
+package app;
+
+import enums.Month;
+
+public class App2 {
+  public static void main(String[] args) {
+    Month m = Month.AUGUST;
+    System.out.println(m.getMonthNum());
+    System.out.println(m);
+  }
+}
