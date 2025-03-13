b53659ea010490c3daad1c8beb7ece128420827a
Mathesu veLi <matheuslevit@gmail.com>
2025-03-12 22:10:41 -0300

    replace a sub string of a string

---
+++ Java/udemy/Carlos Tosin/special-types/string/src/app/Replace.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Replace {
+  public static void main(String[] args) {
+    String str = "java is the greatest language!";
+
+    String restr = str.replace("language", "programming language");
+    System.out.println(restr);
+  }
+}
