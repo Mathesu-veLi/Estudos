63b255d5f2048d1fca207bc1294e7b701c8d05f0
Mathesu veLi <matheuslevit@gmail.com>
2025-03-12 22:08:40 -0300

    get the index of a sub string in a string

---
+++ Java/udemy/Carlos Tosin/special-types/string/src/app/StringIndexOf.java
@@ -0,0 +1,10 @@
+package app;
+
+public class StringIndexOf {
+  public static void main(String[] args) {
+    String str = "java is the greatest language!";
+
+    int pos = str.indexOf("the");
+    System.out.println(pos);
+  }
+}
