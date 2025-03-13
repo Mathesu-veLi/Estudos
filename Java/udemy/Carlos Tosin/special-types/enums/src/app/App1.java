b387c198c325bb3b1e13e1ace59ee9f3cde28a6d
Mathesu veLi <matheuslevit@gmail.com>
2025-03-12 22:47:39 -0300

    creates a basic enum

---
+++ Java/udemy/Carlos Tosin/special-types/enums/src/app/App1.java
@@ -0,0 +1,14 @@
+package app;
+
+public class App1 {
+  private enum Month {
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER,
+    OCTOBER, NOVEMBER, DECEMBER
+  }
+
+  public static void main(String[] args) {
+    Month firstMonth = Month.JANUARY;
+    Month noMonth = null;
+
+  }
+}
