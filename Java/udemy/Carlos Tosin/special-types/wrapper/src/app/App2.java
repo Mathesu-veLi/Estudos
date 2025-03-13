c49aa6c9a86e6df399db3ac8de7e413c96458284
Mathesu veLi <matheuslevit@gmail.com>
2025-03-13 20:04:34 -0300

    parseInt and valueOf

---
+++ Java/udemy/Carlos Tosin/special-types/wrapper/src/app/App2.java
@@ -0,0 +1,12 @@
+package app;
+
+public class App2 {
+  public static void main(String[] args) {
+    Integer i = 1;
+    Integer j = Integer.valueOf("1");
+    int k = Integer.parseInt("1");
+
+    System.out.println(i);
+    System.out.println(j);
+  }
+}
