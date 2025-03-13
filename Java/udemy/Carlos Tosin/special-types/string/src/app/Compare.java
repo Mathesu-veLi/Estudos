c8d5acd186d8a9255d348511c54bdc06da8720b6
Mathesu veLi <matheuslevit@gmail.com>
2025-03-12 22:12:02 -0300

    compare strings

---
+++ Java/udemy/Carlos Tosin/special-types/string/src/app/Compare.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Compare {
+  public static void main(String[] args) {
+    String s1 = "bed";
+    String s2 = "chair";
+
+    int r = s1.compareTo(s2);
+    System.out.println(r);
+  }
+}
