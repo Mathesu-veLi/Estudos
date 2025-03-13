896a3c03de79df522ff5211ab41004fad438fa1b
Mathesu veLi <matheuslevit@gmail.com>
2025-03-13 20:11:33 -0300

    compares integer class wrappers

---
+++ Java/udemy/Carlos Tosin/special-types/wrapper/src/app/App4.java
@@ -0,0 +1,14 @@
+package app;
+
+public class App4 {
+  public static void main(String[] args) {
+    Integer i1 = new Integer(10);
+    Integer i2 = new Integer(10);
+    Integer i3 = 10;
+    Integer i4 = 10;
+
+    System.out.println(i1 == i2);
+    System.out.println(i2 == i3);
+    System.out.println(i3 == i4);
+  }
+}
