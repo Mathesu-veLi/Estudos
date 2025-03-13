edb9ce1936805a24c25a3b371445f457e1ed60fc
Mathesu veLi <matheuslevit@gmail.com>
2025-03-12 21:26:09 -0300

    concatenates string with += in a loop

---
+++ Java/udemy/Carlos Tosin/special-types/string/src/app/App3.java
@@ -0,0 +1,13 @@
+package app;
+
+public class App3 {
+  public static void main(String[] args) {
+    String s = "";
+    for (char c = 'A'; c<= 'Z'; c++) {
+      s += c;
+    }
+    System.out.println(s);
+  }
+
+}
+;
