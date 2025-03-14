7f8131ffeec65167451f753bd32fbf6bfa3d3bf5
Mathesu veLi <matheuslevit@gmail.com>
2025-03-13 22:20:36 -0300

    divide strings in tokens

---
+++ Java/udemy/Carlos Tosin/text-number/src/regex/App5.java
@@ -0,0 +1,27 @@
+package regex;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App5 {
+  public static void main(String[] args) {
+    String text = "a1b2c3d";
+    String[] tokens = text.split("\\d");
+
+    System.out.println(Arrays.toString(tokens));
+
+    Scanner scanner = new Scanner(text);
+    scanner.useDelimiter("\\d");
+    while(scanner.hasNext()) {
+      System.out.println(scanner.next());
+    }
+
+    Scanner scanner2 = new Scanner("1,2,3,4");
+    scanner2.useDelimiter(",");
+
+    while(scanner2.hasNextInt()) {
+      int e = scanner2.nextInt();
+      System.out.println(e);
+    }
+  }
+}
