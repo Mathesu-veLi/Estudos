c9be065a8ad519b4a01f66eb39752f1266e55450
Mathesu veLi <matheuslevit@gmail.com>
2025-03-13 21:41:11 -0300

    gets available currencies and locales

---
+++ Java/udemy/Carlos Tosin/text-number/src/format/App6.java
@@ -0,0 +1,10 @@
+package format;
+
+import java.util.*;
+
+public class App6 {
+  public static void main(String[] args) {
+    System.out.println(Currency.getAvailableCurrencies());
+    System.out.println(Arrays.toString(Locale.getAvailableLocales()));
+  }
+}
