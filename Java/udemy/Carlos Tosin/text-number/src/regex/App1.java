4ff64ba7a4ea23ff2b8117d6de2929d742227e56
Mathesu veLi <matheuslevit@gmail.com>
2025-03-13 21:59:23 -0300

    creates a basic regex

---
+++ Java/udemy/Carlos Tosin/text-number/src/regex/App1.java
@@ -0,0 +1,18 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App1 {
+  public static void main(String[] args) {
+    String text = "abcabc";
+    String regex = "ab";
+
+    Pattern p = Pattern.compile(regex);
+    Matcher m = p.matcher(text);
+    while(m.find()) {
+      System.out.format("%d -> '%s'\n", m.start(), m.group());
+    }
+
+  }
+}
