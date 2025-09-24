d966c4ac9a999732347081279efac658ac881c31
Matheus Levi <matheuslevit@gmail.com>
2025-09-24 19:20:42 -0300

    creates a function to generate a random number

---
+++ Java/udemy/Carlos Tosin/text-number/src/random/RandomApp2.java
@@ -0,0 +1,15 @@
+package random;
+
+public class RandomApp2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int d = random(0,5);
+            System.out.println(d);
+        }
+    }
+
+    private static int random(int from, int to) {
+        int range = to - from + 1;
+        return (int) (Math.random() * range) + from;
+    }
+}
