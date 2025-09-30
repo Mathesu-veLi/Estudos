4d0e83609c586d1a442eac7a7cca33e1865ca7a1
Matheus Levi <matheuslevit@gmail.com>
2025-09-30 09:02:13 -0300

    uses biginteger to store big numbers

---
+++ Java/udemy/Carlos Tosin/text-number/src/biginteger/BigIntegerApp1.java
@@ -0,0 +1,15 @@
+package biginteger;
+
+import java.math.BigInteger;
+
+public class BigIntegerApp1 {
+    public static void main(String[] args) {
+        BigInteger big = new BigInteger("56789073148134914134081348134871134123124352135677975731");
+        big = big.add(BigInteger.TEN);
+        System.out.println(big);
+
+        BigInteger big2 = BigInteger.valueOf(100);
+        System.out.println(big2);
+        System.out.println(big2.intValue());
+    }
+}
