7d071ee268eeb2d4979e43dda8fbf906ba1ac021
Matheus Levi <matheuslevit@gmail.com>
2025-10-01 18:40:29 -0300

    access invalid index in array

---
+++ Java/udemy/Carlos Tosin/collections/src/arrays/ArrayApp3.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class ArrayApp3 {
+    public static void main(String[] args) {
+        String[] array = new String[2];
+        array[0] = "A";
+        array[1] = "B";
+        array[2] = "C";
+    }
+}
