173609d05c4b46253967cbe7e3bb8e87c7fa2a0d
Matheus Levi <matheuslevit@gmail.com>
2025-10-02 20:48:34 -0300

    converts between arrays and lists

---
+++ Java/udemy/Carlos Tosin/collections/src/lists/ListApp4.java
@@ -0,0 +1,16 @@
+package lists;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListApp4 {
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3, 4};
+
+        List<Integer> list = Arrays.asList(array);
+        System.out.println(list);
+
+        Integer[] array2 = list.toArray(new Integer[4]);
+        System.out.println(Arrays.toString(array2));
+    }
+}
