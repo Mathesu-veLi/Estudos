8f2dbb6c826a020f3c8764e2f8b99b45f93b9882
Matheus Levi <matheuslevit@gmail.com>
2025-10-02 21:05:08 -0300

    sort list

---
+++ Java/udemy/Carlos Tosin/collections/src/lists/ListApp7.java
@@ -0,0 +1,12 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListApp7 {
+    public static void main(String[] args) {
+        var list = new ArrayList<>(List.of(6, 5, 7, 1, 3, 2, 4));
+        list.sort(new DescendingComparator());
+        System.out.println(list);
+    }
+}
