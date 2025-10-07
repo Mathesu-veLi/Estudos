3af6bb28109da5bb84b5151dc9707ef5dc8d2dac
Matheus Levi <matheuslevit@gmail.com>
2025-10-07 11:53:38 -0300

    deletes a file using Files class

---
+++ Java/udemy/Carlos Tosin/io/src/file/FileApp4.java
@@ -0,0 +1,13 @@
+package file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileApp4 {
+    public static void main(String[] args) throws IOException {
+        Path file = Path.of("myfile.txt");
+        Files.createFile(file);
+        Files.deleteIfExists(file);
+    }
+}
