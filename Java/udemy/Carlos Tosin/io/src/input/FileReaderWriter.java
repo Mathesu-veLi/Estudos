d4ffa0555dbfff7b76e32cefaa3d93627a59e243
Matheus Levi <matheuslevit@gmail.com>
2025-10-07 21:56:14 -0300

    reads the created file

--- Java/udemy/Carlos Tosin/io/src/input/FileReaderWriter.java
+++ Java/udemy/Carlos Tosin/io/src/input/FileReaderWriter.java
@@ -1,5 +1,6 @@
 package input;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -9,6 +10,16 @@ public class FileReaderWriter {
         try (FileWriter out = new FileWriter("examples/out1.txt", StandardCharsets.UTF_8)) {
             out.write("Hello");
         }
-        ;
+
+        try (FileReader in = new FileReader("examples/out1.txt", StandardCharsets.UTF_8)) {
+            int c = in.read();
+            StringBuilder text = new StringBuilder();
+            while (c > 1) {
+                text.append((char) c);
+                c = in.read();
+            }
+
+            System.out.println(text);
+        }
     }
 }
