ffbb709768cf8f107f15076bf3f68b7059843d5d
Matheus Levi <matheuslevit@gmail.com>
2025-10-14 20:52:54 -0300

    uses IOOperations to write into/read a string

---
+++ Java/udemy/Carlos Tosin/io/src/input/StringReaderWriterApp.java
@@ -0,0 +1,17 @@
+package input;
+
+import java.io.*;
+
+public class StringReaderWriterApp {
+    public static void main(String[] args) throws IOException {
+        try (Writer out = new StringWriter()) {
+            IOOperations.write(out, "Hello!");
+            System.out.println(out);
+        }
+
+        try (Reader in = new StringReader("Uma string qualquer")) {
+            String text = IOOperations.read(in);
+            System.out.println(text);
+        }
+    }
+}
