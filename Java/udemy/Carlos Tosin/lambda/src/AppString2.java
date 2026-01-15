d34b8a25b800f0962ff135f210296b15c80a93b0
Matheus <matheuslevit@gmail.com>
2026-01-14 21:45:44 -0300

    using blocks in a lambda expression

---
+++ Java/udemy/Carlos Tosin/lambda/src/AppString2.java
@@ -0,0 +1,18 @@
+public class AppString2 {
+    public static void main(String[] args) {
+        TextTransformer t = s -> {
+            char[] chars = s.toCharArray();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < chars.length; i += 2) {
+                sb.append(chars[i]);
+            }
+            return sb.toString();
+        };
+
+        print(t, "java rocks!");
+    }
+
+    private static void print(TextTransformer textTransformer, String text) {
+        System.out.println(textTransformer.transform((text)));
+    }
+}
