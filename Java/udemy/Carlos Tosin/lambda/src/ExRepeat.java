20d519e1cc0328e4b8d849eab8df3e421487faf7
Matheus <matheuslevit@gmail.com>
2026-01-14 22:25:42 -0300

    realises exercise

---
+++ Java/udemy/Carlos Tosin/lambda/src/ExRepeat.java
@@ -0,0 +1,16 @@
+public class ExRepeat {
+    public static void main(String[] args) {
+        repeat(5, i -> System.out.println("Number:  " + i));
+    }
+
+    private static void repeat(int n, RepeatCode code) {
+        for(int i = 0; i < n; i++) {
+            code.repeat(i);
+        }
+    }
+
+    @FunctionalInterface
+    interface RepeatCode {
+        void repeat(int index);
+    }
+}
