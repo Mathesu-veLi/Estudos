123dc5cd8275de44ccfe0faead750614afe2940b
Matheus <matheuslevit@gmail.com>
2026-01-14 21:55:24 -0300

    uses method reference to creates a class instance

---
+++ Java/udemy/Carlos Tosin/lambda/src/AppString5.java
@@ -0,0 +1,9 @@
+public class AppString5 {
+    public static void main(String[] args) {
+        createAndPrint(Person::new, "Levi");
+    }
+
+    private static void createAndPrint(PersonCreator creator, String name) {
+        System.out.println(creator.create(name));
+    }
+}
