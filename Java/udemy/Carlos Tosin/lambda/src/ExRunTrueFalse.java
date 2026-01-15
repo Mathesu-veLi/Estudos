8c4dbe2d556f754ba8f3186cc66e4b65338104d3
Matheus <matheuslevit@gmail.com>
2026-01-14 22:40:58 -0300

    realises exercise

---
+++ Java/udemy/Carlos Tosin/lambda/src/ExRunTrueFalse.java
@@ -0,0 +1,23 @@
+public class ExRunTrueFalse {
+    public static void main(String[] args) {
+        run(null, s -> System.out.println("My language is: " + s), () -> System.out.println("My language is none"));
+    }
+
+    private static void run(String s, CodeWithParam codeIfNotNull, CodeWithoutParam codeIfNull) {
+        if(s != null) {
+            codeIfNotNull.execute(s);
+        } else {
+            codeIfNull.execute();
+        }
+    }
+
+    @FunctionalInterface
+    interface CodeWithoutParam {
+        void execute();
+    }
+
+    @FunctionalInterface
+    interface CodeWithParam {
+        void execute(String s);
+    }
+}
