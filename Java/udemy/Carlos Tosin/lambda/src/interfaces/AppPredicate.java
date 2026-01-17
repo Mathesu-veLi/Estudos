c4ffb88d5709d558f7f5b902d888c32e7db5ff0a
Matheus <matheuslevit@gmail.com>
2026-01-16 22:50:02 -0300

    uses the Predicate interface

---
+++ Java/udemy/Carlos Tosin/lambda/src/interfaces/AppPredicate.java
@@ -0,0 +1,22 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class AppPredicate {
+    public static void main(String[] args) {
+        var list = List.of("1", "2", "3", "4", "5", "66");
+        System.out.println(filter(list, s -> s.length() == 1));
+    }
+
+    private static List<String> filter(List<String> list, Predicate<String> predicate) {
+        List<String> newList = new ArrayList<>();
+        for(String e : list) {
+            if(predicate.test(e)) {
+                newList.add(e);
+            }
+        }
+        return newList;
+    }
+}
