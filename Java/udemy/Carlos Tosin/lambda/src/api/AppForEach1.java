a32941979f49c26f2c58ecc240cf5fb9c91bf02f
Matheus <matheuslevit@gmail.com>
2026-01-19 22:53:51 -0300

    uses the forEach method

---
+++ Java/udemy/Carlos Tosin/lambda/src/api/AppForEach1.java
@@ -0,0 +1,11 @@
+package api;
+
+import java.util.List;
+
+public class AppForEach1 {
+    public static void main(String[] args) {
+        var numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        numbers.forEach(System.out::println);
+    }
+}
