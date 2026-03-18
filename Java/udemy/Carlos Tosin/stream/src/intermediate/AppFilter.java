9388cab007aef96c0b756ba3a3c94534c1385352
Matheus <matheuslevit@gmail.com>
2026-03-18 20:19:01 -0300

    using filter in a stream

---
+++ Java/udemy/Carlos Tosin/stream/src/intermediate/AppFilter.java
@@ -0,0 +1,21 @@
+package intermediate;
+
+public class AppFilter {
+    public static void main(String[] args) {
+        var students = Student.list()
+            .stream()
+            .filter(s -> s.yearOfBirth()
+                .getValue() >= 2005)
+            .toList();
+
+        System.out.println(students);
+
+        var grades = Student.list()
+            .stream()
+            .flatMap(s -> s.grades()
+                .stream())
+            .filter(g -> g >= 7.0)
+            .toList();
+        System.out.println(grades);
+    }
+}
