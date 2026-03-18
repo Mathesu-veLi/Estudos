b99001194c1ab139b0c386cc8150d6f8983bbad9
Matheus <matheuslevit@gmail.com>
2026-03-18 19:44:53 -0300

    using Stream.generate to  generate repeated values in a stream

---
+++ Java/udemy/Carlos Tosin/stream/src/basic/App6.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+public class App6 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Stream.generate(() -> 5)
+            .limit(10)
+            .toList();
+
+        System.out.println(numbers);
+
+        double[] array = DoubleStream.generate(() -> 3.5)
+            .limit(3)
+            .toArray();
+        System.out.println(Arrays.toString(array));
+    }
+}
