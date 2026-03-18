d797ee4f6f9ea176befe06417be8c888d3a31195
Matheus <matheuslevit@gmail.com>
2026-03-18 20:22:50 -0300

    using distinct in a stream

---
+++ Java/udemy/Carlos Tosin/stream/src/intermediate/AppDistinct.java
@@ -0,0 +1,14 @@
+package intermediate;
+
+public class AppDistinct {
+    public static void main(String[] args) {
+        var grades = Student.list()
+            .stream()
+            .flatMap(s -> s.grades()
+                .stream())
+            .distinct()
+            .toList();
+
+        System.out.println(grades);
+    }
+}
