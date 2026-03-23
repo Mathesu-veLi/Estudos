9c17fec683fc456235d4d5c726441ef80c9e1cb8
Matheus <matheuslevit@gmail.com>
2026-03-23 17:55:15 -0300

    uses Collectors.groupingBy to count a string stream grouping by the name

---
+++ Java/udemy/Carlos Tosin/stream/src/terminal/AppCollect5.java
@@ -0,0 +1,17 @@
+package terminal;
+
+import java.util.stream.Collectors;
+
+public class AppCollect5 {
+    public static void main(String[] args) {
+        Item
+            .list()
+            .stream()
+            .collect(Collectors.groupingBy(
+                i -> i
+                    .name()
+                    .charAt(0), Collectors.counting()
+            ))
+            .forEach((k, v) -> System.out.println(k + " => " + v));
+    }
+}
